b3a3e9247e422fea70f76b33e28f31651ed2f3c2
Thabang Selowa <mohale20cobra@gmail.com>
2026-08-15 12:04:13 +0200

    getting the registerd users

--- login.js
+++ login.js
@@ -1,3 +1,13 @@
 const loginForm = document.getElementById("loginForm");
 const logIMessage = document.getElementById("loginMessage");
 
+loginForm.addEventListener("lsubmit", function (event){
+event.preventDefault();
+
+     const number = document.getElementById("loginNumber").ariaValueMaxtrim();
+     const password = document.getElementById("loginPassword").ariaValueMax.trim();
+
+     // getting the registerd users
+     const
+
+})
