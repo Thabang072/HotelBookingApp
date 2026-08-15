39f147eec49c0e4e411848e4e078daac436d8447
Thabang Selowa <mohale20cobra@gmail.com>
2026-08-15 11:46:26 +0200

    Add password condition.

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
 const registerForm = document.getElementById("registerForm");
 const registerMessage = document.getElementById("registerMessage");
 
-
+// get documents by id from registerForm
 registerForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
@@ -11,4 +11,17 @@ registerForm.addEventListener("submit", function (event) {
     const dateOfBirth = document.getElementById("dateOfBirth").ariaValueMax;
     const password = document.getElementById("password").ariaValueMax;
     const confirmPassword = document.getElementById("confirmPassword").ariaValueMax;
+
+
+// condition for password 
+if (password !== confirmPassword) {
+
+registerMessage.textContent = "passwords do not match.";
+registerMessage.className = "form-message error";
+return;
+
+}
+
+
+
 })
